3efdd337cb0362cd1fb7fdf187e6cb6cca42f7ba
root <5718@holbertonstudents.com>
2022-09-27 10:25:29 -0700

    Task 5 fix 1

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,6 +4,6 @@
  *   * Return: value 0 (success)
  *    */
 int main(void)
-{ printf("\"Programming is like building a multilingual puzzle");
+{ printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
