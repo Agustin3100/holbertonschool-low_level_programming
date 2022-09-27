2e704fafdcb0ea4255d0eb120be12450c326ada7
root <5718@holbertonstudents.com>
2022-09-27 10:28:11 -0700

    Task 5 fix 3

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,6 +4,6 @@
  *   * Return: value 0 (success)
  *    */
 int main(void)
-{ printf("with proper grammar, but the outcome is a piece of art,");
+{ printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
