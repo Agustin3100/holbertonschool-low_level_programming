0c897b2499c41c1ec9808aa453d6a1b990b47fa4
root <5718@holbertonstudents.com>
2022-09-28 11:41:14 -0700

    task 2 added

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * main - print the alphabet
+ * Return: value 0 (success)
+ */
+
+int main (void) {
+	char a = 'a';
+	char z = 'z';
+		
+         
+
+while (a < z){
+putchar(a);
+	
+
+
+}
+return (0);
+}
