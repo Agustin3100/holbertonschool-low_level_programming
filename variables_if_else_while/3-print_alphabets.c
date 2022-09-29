eae3ee132e05a919a8268d5d5b4042f56fa42b91
root <5718@holbertonstudents.com>
2022-09-29 05:18:38 -0700

    Task 3 added

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/*
+ *  * main - print the alphabet
+ *   * Return: value 0 (success)
+ *    */
+
+
+
+int main()
+{
+	                char ch;
+			char CH;
+
+
+	                  for(ch = 'a'; ch <= 'z';)
+			  {
+															                                putchar(ch);
+			  }
+		         for(CH = 'A'; ch <= 'Z';)
+			 {
+				 putchar(CH);
+				 
+		 	 }			
+			 return 0;
+
+
+}
+                                                             
