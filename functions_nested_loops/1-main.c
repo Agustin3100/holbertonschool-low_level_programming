468b5d977bf62f125e4777c9d4f1c5fa0bc872e4
root <5718@holbertonstudents.com>
2022-09-30 09:08:32 -0700

    Task 1 fix 5

--- functions_nested_loops/1-main.c
+++ functions_nested_loops/1-main.c
@@ -1,12 +1,2 @@
-#include "main.h"
-
-/**
- *  * main - check the code for Holberton School students.
- *   *
- *    * Return: Always 0.
- *     */
+#include <stdio.h>
 int main(void)
-{
-		print_alphabet();
-			return (0);
-}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 #include <stdio.h>
+#include <unistd.h>
 void print_alphabet(void);
-int _putchar(char c);
+
