b33ffae8f64a4f3d943394a8031f4e8b3c80ef42
root <5718@holbertonstudents.com>
2022-09-30 06:09:46 -0700

    Task 9 fix 3

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main (void)
+{
+	int b = '0';
+
+     
+		
+	while(b <= '9')
+	{
+		putchar(b);
+		if ( b != '9')
+		{ 
+		  putchar(',');
+	          putchar (' ');	
+		}
+		b++;
+	      putchar('\n');	
+	    
+	    
+	     
+	     
+	}
+return (0);
+}
+	
+
