1c07cc1cd58f08cc537b8dc59028272989a028d3
root <5718@holbertonstudents.com>
2022-09-30 07:20:19 -0700

    Task 1 fix 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{      char az;
+
+
+	for (az = 'a'; az <= 'z'; az++)
+	{
+
+		putchar(az);
+
+	}
+
+return (0);
+}
+
