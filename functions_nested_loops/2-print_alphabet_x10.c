6fe311083fe7c214b99fb72595a12686f2326d3c
root <5718@holbertonstudents.com>
2022-10-02 07:23:52 -0700

    Task 2 fix 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,31 @@
+#include "main.h"
+void print_alphabet_x10(void)
+{
+        
+          char c;
+	  int a;
+
+               c = 'a';          
+               a = 0;
+                while ( a <= 10)
+                {
+		    
+			
+			
+			
+			for (c = 'a' ; c <= 'z' ; c++)	
+		{      
+			
+			_putchar(c);
+                 
+		  
+                }
+                        _putchar('\n');
+		       	a++;
+
+                        
+                  }
+
+            return;
+
+ }
