fb7df527056dc00612d3187a55757053520a3fd8
root <5718@holbertonstudents.com>
2022-10-02 14:32:06 -0700

    Task 10 fix

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "2abs.c"
+
+int add(int x,int y)
+{
+	
+ 
+  
+	  
+  _2abs(x += y);
+    return(x);
+
+     
+      
+
+}
+
+
+
+
+
+
---
+++ functions_nested_loops/2abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+int _2abs(int x)
+{
+
+
+
+     if (x > 0)
+     {  return(x);
+     }
+     else {
+return(x*-1);
+     }
+}
