3eef331f46db10e8c75aa2d9a69f97faaaeca093
root <5718@holbertonstudents.com>
2022-10-03 08:42:30 -0700

    Betty fix

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,22 +1,28 @@
+/**
+ * print_to_98 - print the alphabet
+ * @n: int for characters
+ *
+ * Description: prints numberz
+ * Return: value 0 (success)
+ */
+
 #include <stdio.h>
 #include "main.h"
-
-
 void print_to_98(int n)
 {
+while (n < 100)
+{
+printf("%d, ", n);
+n++;
 
+if (n >= 98)
+{
 
-while( n < 100 ) {
-   
-      printf("%d, ", n);
-      n++;
-		
-      if( n >= 98) {
-         
-         break;
-      }
-   }
-printf("98\n"); 
-   return;
+break;
+
+}
+}
+printf("98\n");
+return;
 }
 
