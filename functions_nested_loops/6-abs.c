f1bc54da414300b4d998b2a2fd285798bef8bda7
root <5718@holbertonstudents.com>
2022-10-03 07:18:01 -0700

    Betty fix

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,13 +1,18 @@
+/**
+ * _abs - print the alphabet
+ * @x: int for characters
+ *
+ * Description: prints numberz
+ * Return: value 0 (success)
+ */
 #include "main.h"
 int _abs(int x)
 {
-
-     
-
-     if (x > 0)
-     {  return(x); 
-     }
-     else {
-return(x*-1);
-     }	  
-}	
+if (x > 0)
+{  return (x);
+}
+else
+{
+return (x * -1);
+}
+}
