e8f5fc50f7f7c3262896b5037294dde4f95fa84f
root <5718@holbertonstudents.com>
2022-10-03 06:14:10 -0700

    Betty fix

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,29 +1,27 @@
+/**
+ * main - print the alphabet
+ * Description: prints numberz
+ * Return: value 0 (success)
+ */
+
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
-	int b = '0';
+int b = '0';
 
-     
-		
-	while(b <= '9')
-	{
-		putchar(b);
-		if ( b != '9')
-		{ 
-		  putchar(',');
-	          putchar (' ');	
-		}
-		b++;
-	      
-	    
-	    
-	     
-	     
-	
-	}
-	putchar('\n');
+while (b <= '9')
+{
+
+putchar(b);
+
+if (b != '9')
+{
+putchar(',');
+putchar (' ');
+}
+b++;
+}
+putchar('\n');
 return (0);
 }
-	
-
