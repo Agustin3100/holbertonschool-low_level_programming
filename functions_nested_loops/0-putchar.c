0124f7a78abe7ff5615a861ab1884cc9999b0b77
root <5718@holbertonstudents.com>
2022-10-03 06:17:19 -0700

    Betty fix

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,18 +1,22 @@
+/**
+ * main - print the alphabet
+ * Description: prints numberz
+ * Return: value 0 (success)
+ */
+
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
-
-      putchar('_');
-      putchar('p');
-      putchar('u');
-      putchar('t');
-      putchar('c');
-      putchar('h');
-      putchar('a');
-      putchar('r');
-      putchar('\n');
-
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
 
 return (0);
 }
