3aeac72c08578b71e4674ab59bd0bbad1014a98d
root <5718@holbertonstudents.com>
2022-10-03 06:41:55 -0700

    Betty fix

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,15 +1,19 @@
-int _islower(int c)
-{	
-	
-	
+/**
+ * _islower - print the alphabet
+ * @c: int for characters
+ *
+ * Description: prints numberz
+ * Return: value 0 (success)
+ */
 
-	 if(c >= 'A' && c <= 'Z')
- {  
-	 return (0);
+int _islower(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (0);
 }
 else
 {
-     return (1);    
+return (1);
 }
 }
-
