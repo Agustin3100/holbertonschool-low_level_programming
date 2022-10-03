c4bb649f7f3b81b611bfdee6ea1157b709b1683d
root <5718@holbertonstudents.com>
2022-10-03 07:22:39 -0700

    Betty fix

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,22 +1,17 @@
+/**
+ * add - print the alphabet
+ * @x: int for characters
+ * @y: int for characters
+ *
+ * Description: prints numberz
+ * Return: value 0 (success)
+ */
+
 #include "main.h"
 #include "2abs.c"
 
-int add(int x,int y)
+int add(int x, int y)
 {
-	
- 
-  
-	  
-  _2abs(x += y);
-    return(x);
-
-     
-      
-
+_2abs(x += y);
+return (x);
 }
-
-
-
-
-
-
