76b9b6c764fc67b170f5c00195730b8638882901
root <5718@holbertonstudents.com>
2022-10-03 07:08:57 -0700

    Betty fix

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,37 +1,39 @@
+/**
+ * print_sign - print the alphabet
+ * @n: int for characters
+ *
+ * Description: prints numberz
+ * Return: value 0 (success)
+ */
+
 #include "main.h"
 int print_sign(int n)
 {
-     char c;
-     char d;
-     int i;
+	char c;
+	char d;
+	int i;
 
-     c = '-';
-     d = '+';
-     i = 0;
-{
-     if ( n < 0)
-	{ 
+	c = '-';
+	d = '+';
+	i = 0;
+	{
+	if (n < 0)
+	{
 		_putchar(c);
-                return (-1);
+		return (-1);
 
-	}		
-     else if( n == 0){
-            
-	     _putchar(i);
-             return(0);    
-     
-     }
-    else{
-       
-	    _putchar(d);
-              return(1);
+	}
+	else if (n == 0)
+	{
+		_putchar(i);
+		return (0);
+	}
+	else
+	{
 
-             }
+	_putchar(d);
+	return (1);
 
-
-
-
-
-
-} 
-}
+	}
+	}
+	}
