2db165ec88c53d92bb9f95417140531df8309c61
root <5718@holbertonstudents.com>
2022-10-03 06:45:47 -0700

    Betty fix

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,19 +1,25 @@
+/**
+ * _isalpha - print the alphabet
+ * @c: int for characters
+ *
+ * Description: prints numberz
+ * Return: value 0 (success)
+ */
+
 int _isalpha(int c)
 {
 
-if(c >= 'A' && c <= 'Z')
-    {
-       return (1);
-    }
-    else if(c >= 'a' && c <= 'z')
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else if (c >= 'a' && c <= 'z')
 
-    {
-          return (1);
-    }
-    else
-    {
-      return (0);
-    }
-
-
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
