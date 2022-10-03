4ca4f9b35adfde985d7923ced2f2729076a28d55
root <5718@holbertonstudents.com>
2022-10-03 06:20:26 -0700

    Betty fix

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,17 @@
+/**
+ * print_alphabet - print the alphabet
+ * Description: prints numberz
+ * Return: value 0 (success)
+ */
+
 #include "main.h"
 void print_alphabet(void)
 {
+char c;
 
-
-	char c;
-
-	for (c = 'a' ; c <= 'z' ; c++)
-	{	_putchar(c);
-	}
-	_putchar('\n');
+for (c = 'a' ; c <= 'z' ; c++)
+{	_putchar(c);
+}
+_putchar('\n');
 return;
 }
