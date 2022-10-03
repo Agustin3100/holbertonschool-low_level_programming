e2fb32fe6c81bb603634078657409d23757d652c
root <5718@holbertonstudents.com>
2022-10-03 06:34:31 -0700

    Betty fix

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,31 +1,31 @@
 #include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabet
+ * Description: prints numberz
+ * Return: value 0 (success)
+ */
+
 void print_alphabet_x10(void)
 {
-        
-          char c;
-	  int a;
+char c;
+int a;
 
-               c = 'a';          
-               a = 0;
-                while ( a < 10)
-                {
-		    
-			
-			
-			
-			for (c = 'a' ; c <= 'z' ; c++)	
-		{      
-			
-			_putchar(c);
-                 
-		  
-                }
-                        _putchar('\n');
-		       	a++;
+c = 'a';
+a = 0;
+while (a < 10)
+{
 
-                        
-                  }
+for (c = 'a' ; c <= 'z' ; c++)
 
-            return;
+{
 
- }
+_putchar(c);
+
+}
+_putchar('\n');
+a++;
+}
+
+return;
+
+}
