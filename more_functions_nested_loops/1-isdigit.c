5325f836366a4b4dd6a3722b0df514eaea990171
root <5718@holbertonstudents.com>
2022-10-04 15:40:55 -0700

    Task 1 fix 3

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -8,13 +8,16 @@ else if (c >= 'A' && c <= 'Z')
 {
 return (0);
 }
-else if (c >= 0 && c <= 9)
+else if(c >= 36 && c <= 136)
+{	return(1);
+}
+else if (c >= 0 || c <= 9)
 {
 return (1);
 }
 
 else
-{ return (1);
+{ return (0);
 
 
 }
