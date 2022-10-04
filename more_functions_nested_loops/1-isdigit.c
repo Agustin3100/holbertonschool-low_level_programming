f428bf61ce8b3cfc69e1fb41d49f9182fcc7c42f
root <5718@holbertonstudents.com>
2022-10-04 15:33:49 -0700

    Task 1 fix 2

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,21 @@
 int _isdigit(int c)
 {
-   if( c == 'a')
-   {    return (1);
-   }	
-		   else{
-		return (0);	   
-               }
+   if ( c >= 'a' && c <= 'z')
+   {    return (0);
+   		   
+   }
+else if (c >= 'A' && c <= 'Z')
+{
+return (0);
+}
+else if (c >= 0 && c <= 9)
+{
+return (1);
+}
+
+else
+{ return (1);
 
 
-
-
-
+}
 }
