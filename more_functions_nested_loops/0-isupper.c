e1304d78438e47b7848fb346e7e39d2cef94f414
root <5718@holbertonstudents.com>
2022-10-04 10:21:35 -0700

    Task 0 added

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+/**
+ * _isupper - print the alphabet
+ * @c: int for characters
+ *
+ * Description: prints numberz
+ * Return: value 0 (success)
+ */
+
+int _isupper(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (0);
+}
+else
+{
+return (1);
+}
+}
