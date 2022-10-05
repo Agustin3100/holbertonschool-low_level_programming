13604ac3bc2538ed3551994456a0c07bf4ea7ba0
root <5718@holbertonstudents.com>
2022-10-05 09:38:14 -0700

    Task 8 added

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+void print_square(int size)
+{
+
+int x,y;
+
+
+
+
+        if ( size > 0)
+        {
+          for ( x = 1; x <= size; x++)
+          {
+           for( y = 1; y < size; y++)
+           {
+         _putchar(35);
+
+
+
+                           }
+                 _putchar(35);
+                 _putchar('\n');
+                 }
+        }
+        else{
+
+        _putchar('\n');
+        }
+}
