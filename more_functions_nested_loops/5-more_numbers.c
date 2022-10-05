bea2c7cc2b271d2b95c684f658cab99ae04bca2a
root <5718@holbertonstudents.com>
2022-10-05 05:44:17 -0700

    Task 5 fix 3

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,14 +5,17 @@ void more_numbers(void)
 
 for (m = 0; m <= 9; m++)
 {
-for (n = 0; n <= 14; n++)
-{
-if (n >= 10)
-{
-_putchar(m / 10 + '0');
+	for (n = 0; n <= 14; n++)
+	{
+		if (n >= 10)
+		{
+			_putchar(n / 10 + '0');
+		}
+	_putchar(n % 10 + '0');
+        
+	}
+	_putchar('\n');
+
 }
-_putchar(n % 10 + '0');
-}
-_putchar('\n');
-}
+
 }
