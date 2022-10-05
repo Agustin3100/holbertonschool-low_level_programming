05668e5b8ec91e58f5b910de4d925cf3c592aec6
root <5718@holbertonstudents.com>
2022-10-05 06:35:11 -0700

    Task 6 fix 1

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,18 +1,20 @@
 #include "main.h"
 void print_line(int n)
 {	
-   n = 0;
-	
-        while ( n < 0 )
-	{	_putchar('_');
-		_putchar('\n');
+   
+ 
+
+  	
+      
         while ( n > 0)
-	{       _putchar('_');
-		_putchar('\n');	
+	{       
+		_putchar('_');
+		--n;
 
 	} 	
-	}
-	if ( n == 0)
+	
+	if ( n <= 0)
         {    _putchar('\n');
          }
 }
+
