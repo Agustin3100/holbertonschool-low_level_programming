08b4be27427f1ac6eaab6710198533466f665b4b
root <5718@holbertonstudents.com>
2022-10-06 08:30:33 -0700

    Task 1 added

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+/**
+ * swap_int - print the alphabet
+ * @a: int for characters
+ * @b: int pointer
+ *
+ * Description: prints numberz
+ * Return: value 0 (success)
+ */
+void swap_int(int *a, int *b)
+{
+int c;
+
+c = *a;
+*a = *b;
+*b = c;
+}
