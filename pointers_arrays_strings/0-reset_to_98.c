8ed4482f539cc212f4e6867b2c49051a68a9de89
root <5718@holbertonstudents.com>
2022-10-06 08:17:46 -0700

    Task 0 added

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * reset_to_98 - dereferences pointer
+ * @n: pointer dereferencer
+ *
+ * Description: updates value of pointer
+ * Return: value 0 (success)
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
