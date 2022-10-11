429e6e58b17800fa8752607adfd3ac9b3502966c
root <5718@holbertonstudents.com>
2022-10-11 13:38:29 -0700

    Betty fix

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,14 +1,21 @@
+/**
+ * *_strcpy - copies a string from src to dest
+ * @dest: char string for example
+ * @src:  char string for example
+ *
+ * Description: updates value of pointer
+ * Return: value 0 (success)
+ */
 #include "main.h"
 #include <stdio.h>
 char *_strcpy(char *dest, char *src)
 {
- int i;
+	int i;
 
- for (i = 0; src[i] >= '\0'; ++i) {
-        
-        dest[i] = src[i];
-    }
-  
-    dest[i] = '\0';
-    return (dest);
+	for (i = 0; src[i] >= '\0'; ++i)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
