bf504cf058b975f252b8cdf916f4959966649e39
root <5718@holbertonstudents.com>
2022-10-11 12:54:34 -0700

    Betty fix

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,7 @@
 /**
  * puts2 - prinst the content of the string
  *@str: print the even contents of a string
+ *Qi: counter
  *
  * Description: updates value of pointer
  *Return: void
@@ -9,15 +10,32 @@
 #include <stdio.h>
 void puts2(char *str)
 {
+	int i = 0;
+	int cont = _strlen(str);
 
-int i;
-i = 0;
-	if (i % 2 == 0)
-	 {
-        while (*str)
-	 {
-	 _putchar(*str);
-	str = str + 2;
-	     }
-	 }
+	for (; i < cont; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
+
+/**
+ * _strlen - prints the lenght of a string
+ * @s: char pointer for example
+ *
+ * Description: updates value of pointer
+ * Return: value 0 (success)
+ */
+int _strlen(char *s)
+{
+	int lt = 0;
+
+	while (*s)
+	{
+		lt++;
+		s = s + 1;
+	}
+	return (lt);
 }
--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,18 +1,26 @@
 #include "main.h"
+/**
+ * puts_half - prints the half of a string
+ * @s: char pointer for example
+ *
+ * Description: updates value of pointer
+ * Return: value 0 (success)
+ */
 int _strlen(char *s);
 
 void puts_half(char *str)
 {
 int i;
 	int lt = _strlen(str) - 1;
-	int lh = (lt -1) / 2;
+	int lh = (lt - 1) / 2;
 
 	if ((lt - 1) % 2 != 0)
 	{
 		lh++;
-	
+
 	}
-	else{
+	else
+	{
 		str += lh + 1;
 	}
 	for (i = lh; i < lt; i++)
@@ -24,10 +32,13 @@ int i;
 }
 
 
-
-
-
-
+/**
+ * _strlen - prints the lenght of a string
+ * @s: char pointer for example
+ *
+ * Description: updates value of pointer
+ * Return: value 0 (success)
+ */
 int _strlen(char *s)
 {
 	int lt = 0;
@@ -37,5 +48,5 @@ int _strlen(char *s)
 		lt++;
 		s = s + 1;
 	}
-	return (lt;
+	return (lt);
 }
