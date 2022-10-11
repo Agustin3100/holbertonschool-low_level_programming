84da5697d865c57fbfdb89280bd7770981ab5619
root <5718@holbertonstudents.com>
2022-10-11 16:21:32 -0700

    Added comments

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,25 +2,21 @@
 int _strlen(char *s);
 char *_strcat(char *dest, char *src)
 {
- int i;
-  
-    int j = _strlen(src) -1;
-  
-    for (i = 0; src[i] != '\0'; i++) {
-        dest[i + j] = src[i];
-    }
-    dest[i + j] = '\0';
-return (dest);
-}
+int i = 0;  /*contador para la copia*/
+	int j = 0; /* contador para el source*/
+		  
+	char *aux = dest; /* variable auxiliar con el valor de dest para el return*/
+        char *aux2 = src; /*variable aux para el puntero de src ( para la copia) */
 
-int _strlen(char *s)
-{
-        int lt = 0;
-
-        while (*s)
-        {
-                lt++;
-                s = s + 1;
-        }
-        return (lt);
-}
+	while (*dest) /* while para printear el content de dest */
+		dest++;
+	while (*src++) /* while para printear el content de src */
+		j++;
+	while (i < j) /* while de la copia a partir del inicio de src con su puntero para copiar el valor de src a traves de aux 2 hacia dest*/
+	{
+		*dest++ = *aux2++; 
+		i++;
+	}
+	*dest = '\0'; 
+	return (aux); 
+}      	
