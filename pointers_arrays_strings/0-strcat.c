83423e1cd2222cfcbfa7ea7f84a6ae52063664d0
root <5718@holbertonstudents.com>
2022-10-12 05:29:43 -0700

    Task 0 description added and betty fix

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,31 @@
+/**
+ * *_strcat - copies a string
+ * @dest: dest pointer for example
+ * @src: src pointer fox example
+ *
+ * Description: updates value of pointer
+ * Return: destination string (success)
+ */
 #include "main.h"
-int _strlen(char *s);
+
 char *_strcat(char *dest, char *src)
 {
 int i = 0;  /*contador para la copia*/
 	int j = 0; /* contador para el source*/
-		  
-	char *aux = dest; /* variable auxiliar con el valor de dest para el return*/
-        char *aux2 = src; /*variable aux para el puntero de src ( para la copia) */
 
+	char *aux = dest; /* puntero auxiliar con el valor de dest para el return*/
+	char *aux2 = src;
+	/* puntero aux para el puntero de src */
 	while (*dest) /* while para printear el content de dest */
 		dest++;
 	while (*src++) /* while para printear el content de src */
 		j++;
-	while (i < j) /* while de la copia a partir del inicio de src con su puntero para copiar el valor de src a traves de aux 2 hacia dest*/
+	while (i < j) /* while de la copia a partir de src
+*/
 	{
-		*dest++ = *aux2++; 
+		*dest++ = *aux2++;
 		i++;
 	}
-	*dest = '\0'; 
-	return (aux); 
-}      	
+	*dest = '\0';
+	return (aux);
+}
