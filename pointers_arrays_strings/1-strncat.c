15f250fa04b0f440882a231b097595053c4349e8
root <5718@holbertonstudents.com>
2022-10-12 09:53:04 -0700

    Task 1 fix 1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,16 +1,24 @@
+/**
+ * *_strncat - concatenates two string depending of n bytes
+ * @dest: dest pointer for example
+ * @src: src pointer for example
+ * @n: int with the total lenght of the strings
+ *
+ * Description: updates value of pointer
+ * Return: value 0 (success)
+ */
 #include "main.h"
+
 char *_strncat(char *dest, char *src, int n)
 {
-        int i;
-        int j;
-
-        for (i = 0 ; dest[i] != '\0'; i++)
-             ;   
-        
-	for (j = 0 ; src[j] != src[n] ; j++)
-        {
-                dest[i] = src[j];
-                i++;
-        }
-        return (dest);
+	int i;
+	int j;
+		for (i = 0 ; dest[i] != '\0'; i++)
+		;
+		for (j = 0 ; src[j] != src[n] ; j++)
+		{
+		dest[i] = src[j];
+		i++;
+				}
+	return (dest);
 }
