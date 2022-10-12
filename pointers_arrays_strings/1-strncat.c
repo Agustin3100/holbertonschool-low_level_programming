95530687f122d649e5bfb0730182a020abeaf6d4
root <5718@holbertonstudents.com>
2022-10-12 09:43:33 -0700

    Task 1 fix 1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,15 +1,16 @@
 #include "main.h"
-char *_strncat(char *dest, char *src, int n){
-	
-	   int i;
-	   int j;
+char *_strncat(char *dest, char *src, int n)
+{
+        int i;
+        int j;
 
-	for (i = 0 ; dest[i] != '\0' ; i++)
-		;
-	for (j = 0 ; j < n && src[j] != '\0' ; j++)
-	{
-		dest[i] = src[j];
-		j++;
-	}
-	return (dest);
+        for (i = 0 ; dest[i] != '\0'; i++)
+             ;   
+        
+	for (j = 0 ; src[j] != src[n] ; j++)
+        {
+                dest[i] = src[j];
+                i++;
+        }
+        return (dest);
 }
