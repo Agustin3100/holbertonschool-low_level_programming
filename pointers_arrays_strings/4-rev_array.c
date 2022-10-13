5980daa87555a0f2652ba534379c60e8e544fe57
root <5718@holbertonstudents.com>
2022-10-12 17:07:47 -0700

    Task 4 betty fix

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,6 @@
 /**
  * reverse_array - reverse an int array
- * @a: pointer for example 
+ * @a: pointer for example
  * @n: int representing the numbers of elemnts of the array
  *
  * Description: prints numberz
@@ -8,18 +8,13 @@
  */
 void reverse_array(int *a, int n)
 {
-   
-  
-   int i, temp;
-   
-   
-  for( i = 0; i < n -1; i++)
-  {
-        temp = a[n - 1];
-	a[n -1] = a[i];
-	a[i] = temp;
-        n--;
-  }
-  	
+	int i, temp;
 
-}  
+	for (i = 0; i < n - 1; i++)
+	{
+		temp = a[n - 1];
+		a[n - 1] = a[i];
+		a[i] = temp;
+		n--;
+	}
+}
