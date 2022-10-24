d2afd1a972c657972d2683303b6814ae6b6ca674
root <5718@holbertonstudents.com>
2022-10-24 10:13:56 -0700

    Task 1 fix 3

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 
@@ -24,19 +25,22 @@ if(p == NULL)
 {	return (NULL);
 }
 
-if(s2len > n)
+if(s2len >= n)
 {	s2len = n;
+	
 }
 
 for( i = 0; s1[i]; i++)
 {
 	p[i] = s1[i];
+	
 }
 
 for(i = 0; s2[i] != s2[n]; i++)
 {
 	
-	p[s1len + i] = s2[i];	
+	p[s1len + i] = s2[i];
+	
 }
 
 
