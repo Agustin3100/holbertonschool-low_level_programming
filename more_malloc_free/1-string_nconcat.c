7d8fcee867e40e4314ae9222ab3738b63e9ad198
root <5718@holbertonstudents.com>
2022-10-24 10:37:34 -0700

    Task 1 fix 4

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,35 +13,39 @@ char *p;
 
 p = malloc(sizeof(char)* len);
 
-if( s1 == NULL )
-{	return "";
-}
+	if( s1 == NULL )
+	{	
+		return "";
+	}
 
-if(s2 == NULL)
-{
+	if(s2 == NULL)
+	{
 	return "";
-}
-if(p == NULL)
-{	return (NULL);
-}
+	}
 
-if(s2len >= n)
-{	s2len = n;
-	
-}
+	if(p == NULL)
+	{	
+	return (NULL);
+	}
 
-for( i = 0; s1[i]; i++)
-{
-	p[i] = s1[i];
+	if(s2len >= n)
+	{		
+		s2len = n;
 	
-}
+	}
 
-for(i = 0; s2[i] != s2[n]; i++)
-{
+		for( i = 0; s1[i]; i++)
+		{
+		p[i] = s1[i];
 	
-	p[s1len + i] = s2[i];
+		}	
+
+		for(i = 0; s2[i] != s2[n]; i++)
+		{
 	
-}
+		p[s1len + i] = s2[i];
+	
+		}
 
 
 
