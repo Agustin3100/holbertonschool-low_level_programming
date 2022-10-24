718c021c974316d689aba285e2f11fbb30460142
root <5718@holbertonstudents.com>
2022-10-24 09:33:26 -0700

    Task 0 betty fix

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,20 @@
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: unsigned int for example
+ *
+ * Description: updates value of pointer
+ * Return: value 0 (success)
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 
-void *malloc_checked(unsigned int b){
-	
-	unsigned int *p;	
-		
-	
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *p;
 
 	p = malloc(b);
-
-if(p == NULL)
-
-exit(98);
-
-
-	return p;
-
+	if (p == NULL)
+	exit(98);
+	return (p);
 }
