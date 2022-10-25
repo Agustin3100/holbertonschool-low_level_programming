386e7757175c9dd9c048b64bd504d79c1767f387
root <5718@holbertonstudents.com>
2022-10-25 07:27:36 -0700

    Task 1 betty fix

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,28 +1,37 @@
+/**
+ * _strdup - prints the lenght of a string
+ * @str: char pointer for example
+ *
+ * Description: updates value of pointer
+ * Return: NULL if malloc fails or pointer on success
+ */
 #include <stdio.h>
 #include <stdlib.h>
 char *_strdup(char *str)
 {
-char *p;
-unsigned int i;
-unsigned int j;
+	char *p;
+	unsigned int i;
+	unsigned int j;
 
-if( str == NULL)
-{	return(NULL);
-}
-for(i = 0; str[i]; i++)
-;
-p = malloc(sizeof(char)* i + 1);  /*This loop and malloc to reserve the same amount of memory for the copy**/
+	if (str == NULL)
+	{
+	return (NULL);
+	}
 
-if(p == NULL)
-{	return(NULL);
-}
-for(j = 0; str[j]; j++)
-{  
-       	p[j] = str[j];
-	
-}
+	for (i = 0; str[i]; i++)
+		;
 
-return (p);
+	p = malloc(sizeof(char) * i + 1);
 
+	if (p == NULL)
+	{
+		return (NULL);
+	}
 
+	for (j = 0; str[j]; j++)
+	{
+		p[j] = str[j];
+	}
+
+	return (p);
 }
