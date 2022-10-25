73c2db0139589b9a753592223b939311136d592a
root <5718@holbertonstudents.com>
2022-10-25 10:28:57 -0700

    Task 2 fix 4

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,3 +1,11 @@
+/**
+ * str_concat - concatenates two strings
+ * @s1: char pointer for example
+ * @s2: char pointer for example
+ *
+ * Description: updates value of pointer
+ * Return: NULL if p s1 or s2 is NULL
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,28 +20,26 @@ char *p;
 
 
 
-p = malloc(sizeof(char)* len);
+p = malloc(sizeof(char) * len);
 
-if(p == NULL)
-{
-	return NULL;
-}
+	if (p == NULL)
+	{
+		return (NULL);
+	}
 
-if(s1 == NULL || s2 == NULL)
-{
-	return NULL;
-}
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
 
-for( i = 0; s1[i]; i++)
-{
-	p[i] = s1[i];	
-}
-for(i = 0; s2[i]; i++)
-{
-	p[s1len + i] = s2[i];
-}
-
-return p;
-
-
+	for( i = 0; s1[i]; i++)
+	{
+		p[i] = s1[i];	
+	}
+	for(i = 0; s2[i]; i++)
+	{
+		p[s1len + i] = s2[i];
+	}
+	
+return (p);
 }
