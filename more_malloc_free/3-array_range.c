087f661b0719bfe84d5cec46a879f7313454fbfb
root <5718@holbertonstudents.com>
2022-10-26 19:04:19 -0700

    Task 3 betty fix

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,45 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * array_range - concatenates two strings considering n bytes
+ * @max: string 1
+ * @min: string 2
+ *
+ * Description: updates value of pointer
+ * Return: NULL if malloc fails
+ */
 int *array_range(int min, int max)
 {
+	int i, j;
+	int *p;
 
-        int i,j;
-        int *p;
-	
-	
-	
-	
 	j = max - min + 1;
 
-	if(min > max)
+	if (min > max)
 	{
-		return NULL;
-	
+		return (NULL);
 	}
 
-	
+	p = malloc(sizeof(int) * (max - min + 1));
 
-        p = malloc(sizeof(int)* (max - min +1));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
 
-	
-	if(p == NULL)
-	     {
-		return NULL;
-	     }
-
-
-
-	for(i = 0; i < j; i++)
+	for (i = 0; i < j; i++)
 	{
 		p[i] = min;
 		min++;
 	}
-
-
-return p;
-
-
-
-
-
+	return (p);
 }
