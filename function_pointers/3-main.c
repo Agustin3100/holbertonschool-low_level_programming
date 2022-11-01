41026f27ef874b4d22b4107f79c561a9a23d173a
root <5718@holbertonstudents.com>
2022-11-01 14:35:11 -0700

    Task 3 betty fix

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 #include "3-calc.h"
+/**
+ * main - prints the result of the requesting operations
+ * @argv: input from the user
+ * @argc: number of arguments
+ *
+ * Description: updates value of pointer
+ * Return: value 0 (success)
+ */
 int main(int argc, char *argv[])
 {
 
-int a,b,c;
+int a, b, c;
 char *op;
 
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-if(argc != 4)
-{
-	printf("Error\n");
-	exit(98);
-}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op = argv[2];
 
-a = atoi(argv[1]);
-b = atoi(argv[3]);
-op = argv[2];
+	if (*op < 42 && *op > 47)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	c = (*get_op_func)(op)(a, b);
 
-if(*op < 42 && *op > 47)
-{
-	printf("Error\n");
-	exit(99);
-
-}
-
-c = (*get_op_func)(op)(a,b);
-
-	 
-
-	printf("%d\n",c);
-
-	
-			
-return 0;
+	printf("%d\n", c);
+	return (0);
 }
