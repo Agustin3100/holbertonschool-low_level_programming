c37a52e5eb50e0cd06553ed4baa6fd31edb3f063
root <5718@holbertonstudents.com>
2022-11-19 05:04:57 -0800

    Task 5 while added

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * free_listint2 - frees a single link list and sets the head to null
  * @head: dereferenced pointer to node value
@@ -9,9 +10,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	
+
+	while (*head)
+	{
+
+
+
 		tmp = *head;
 		*head = tmp->next;
 		free(tmp);
-	
+	}
+
 }
--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -2,7 +2,27 @@
 #include <stdlib.h>
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+listint_t *nexth;
+
+	nexth = malloc(sizeof(listint_t));
 	
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	for (idx = 0; head; idx++)
+	{	
+		 
+		
+			nexth->n = n;
+			nexth->next = *head;
+		
+
+		*head = (*head)->next;
+		
+	}
+	return (nexth);	
 
 
 
