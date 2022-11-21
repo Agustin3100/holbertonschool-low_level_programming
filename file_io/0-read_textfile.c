688f21193376bf3051e4dbfacbc0d8d858779b2e
root <5718@holbertonstudents.com>
2022-11-21 09:32:09 -0800

    Task 0 return in error added and betty fixed

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -5,23 +5,31 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "main.h"
+/**
+ * read_textfile - reads a file and prints its content to stdout
+ * @filename: path name to the requested file
+ * @letters: total letters on the file
+ *
+ * Description: updates value of pointer
+ * Return: the actual number of letters it could read or print
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	
-	
 	char *buf = malloc(letters);
-	int fd,wr,rd;
+	int fd, wr, rd;
 
 	fd = open(filename, O_RDONLY);
-	
 
-	
-	 rd = read(fd,buf,letters);
-	
-	wr = write(STDOUT_FILENO,buf,rd);
-	
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	rd = read(fd, buf, letters);
+
+	wr = write(STDOUT_FILENO, buf, rd);
+
 	close(fd);
-	
-	
+
 	return (wr);
 }
